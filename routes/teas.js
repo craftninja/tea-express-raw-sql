31834f570a171fb1b668f167cd4bb05a2b2bf2f6
EmilyPlatzer <emilyplatzer@gmail.com>
2015-06-12 11:55:23 -0600

    User can add new teas

--- routes/teas.js
+++ routes/teas.js
@@ -20,4 +20,25 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('teas/new')
+})
+
+router.post('/', function(req, res, next) {
+  pg.connect(connString, function(err, client, done) {
+    if (err) return console.log(err);
+    var reorderable;
+    if (req.body['tea[reorderable]']) {
+      reorderable = true;
+    } else {
+      reorderable = false;
+    };
+    var query = client.query("INSERT INTO teas(name, country, price, reorderable) VALUES($1, $2, $3, $4)", [req.body['tea[name]'], req.body['tea[country]'], req.body['tea[price]'], reorderable]);
+    query.on('end', function() {
+      done();
+      res.redirect('/teas');
+    });
+  });
+});
+
 module.exports = router;

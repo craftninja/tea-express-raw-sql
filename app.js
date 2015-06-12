b3aead4064f8766c52b7f8e2a3a259d9c46d9899
EmilyPlatzer <emilyplatzer@gmail.com>
2015-06-12 10:09:19 -0600

    User can visit '/teas' path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ client.connect(function(err) {
 });
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var teas = require('./routes/teas');
 
 var app = express();
 
@@ -40,7 +40,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/teas', teas);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
